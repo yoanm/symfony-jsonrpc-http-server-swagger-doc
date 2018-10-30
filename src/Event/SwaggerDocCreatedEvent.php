20756b168391ac313e3b7d9c97cd4aa893329ce3
Yoanm <yoanm@users.noreply.github.com>
2018-10-30 05:24:28 +0100

    Improve

---
+++ src/Event/SwaggerDocCreatedEvent.php
@@ -0,0 +1,56 @@
+<?php
+namespace Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\Event;
+
+use Yoanm\JsonRpcServerDoc\Model\HttpServerDoc;
+use Yoanm\SymfonyJsonRpcHttpServerDoc\Event\DocEvent;
+
+/**
+ * Class SwaggerDocCreatedEvent
+ */
+class SwaggerDocCreatedEvent extends DocEvent
+{
+    const EVENT_NAME = 'json_rpc_http_server_swagger_doc.array_created';
+
+    /** @var array */
+    private $swaggerDoc;
+    /** @var HttpServerDoc|null */
+    private $serverDoc;
+
+    /**
+     * @param array                     $swaggerDoc
+     * @param HttpServerDoc|null $serverDoc
+     */
+    public function __construct(array $swaggerDoc, HttpServerDoc $serverDoc = null)
+    {
+        $this->swaggerDoc = $swaggerDoc;
+        $this->serverDoc = $serverDoc;
+    }
+
+    /**
+     * @return HttpServerDoc
+     */
+    public function getSwaggerDoc()
+    {
+        return $this->swaggerDoc;
+    }
+
+    /**
+     * @return HttpServerDoc|null
+     */
+    public function getServerDoc()
+    {
+        return $this->serverDoc;
+    }
+
+    /**
+     * @param array $swaggerDoc
+     *
+     * @return SwaggerDocCreatedEvent
+     */
+    public function setSwaggerDoc(array $swaggerDoc)
+    {
+        $this->swaggerDoc = $swaggerDoc;
+
+        return $this;
+    }
+}
---
+++ src/Provider/DocProvider.php
@@ -0,0 +1,60 @@
+<?php
+namespace Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\Provider;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Yoanm\JsonRpcHttpServerSwaggerDoc\Normalizer\DocNormalizer;
+use Yoanm\SymfonyJsonRpcHttpServerDoc\Creator\HttpServerDocCreator;
+use Yoanm\SymfonyJsonRpcHttpServerDoc\Provider\DocProviderInterface;
+use Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\Event\SwaggerDocCreatedEvent;
+
+/**
+ * Class DocProvider
+ */
+class DocProvider implements DocProviderInterface
+{
+    /** @var EventDispatcherInterface */
+    private $dispatcher;
+    /** @var HttpServerDocCreator */
+    private $serverDocCreator;
+    /** @var DocNormalizer */
+    private $docNormalizer;
+
+    /**
+     * @param EventDispatcherInterface $dispatcher
+     * @param HttpServerDocCreator     $serverDocCreator
+     * @param DocNormalizer            $docNormalizer
+     */
+    public function __construct(
+        EventDispatcherInterface $dispatcher,
+        HttpServerDocCreator $serverDocCreator,
+        DocNormalizer $docNormalizer
+    ) {
+        $this->dispatcher = $dispatcher;
+        $this->serverDocCreator = $serverDocCreator;
+        $this->docNormalizer = $docNormalizer;
+    }
+
+    /**
+     * @param string|null $host
+     *
+     * @return array
+     */
+    public function getDoc($host = null)
+    {
+        $rawDoc = $this->serverDocCreator->create($host);
+        $swaggerDoc = $this->docNormalizer->normalize($rawDoc);
+
+        $event = new SwaggerDocCreatedEvent($swaggerDoc, $rawDoc);
+        $this->dispatcher->dispatch($event::EVENT_NAME, $event);
+
+        return $event->getSwaggerDoc();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($filename, $host = null)
+    {
+        return 'swagger.json' === $filename;
+    }
+}
