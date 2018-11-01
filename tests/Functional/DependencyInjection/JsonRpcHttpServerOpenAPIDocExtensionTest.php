7dae829def667d2eb3bb4805be2e024e2e914ecb
Yoanm <yoanm@users.noreply.github.com>
2018-11-01 21:57:52 +0100

    Add FT & UT

---
+++ tests/Functional/DependencyInjection/JsonRpcHttpServerOpenAPIDocExtensionTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tests\Functional\DependencyInjection;
+
+use Tests\Common\DependencyInjection\AbstractTestClass;
+use Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\DependencyInjection\JsonRpcHttpServerSwaggerDocExtension;
+
+/**
+ * @covers \Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\DependencyInjection\JsonRpcHttpServerSwaggerDocExtension
+ */
+class JsonRpcHttpServerSwaggerDocExtensionTest extends AbstractTestClass
+{
+    public function testShouldBeLoadable()
+    {
+        $this->load();
+
+        $this->assertDocProviderIsLoadable();
+    }
+
+    public function testShouldReturnAnXsdValidationBasePath()
+    {
+        $this->assertNotNull((new JsonRpcHttpServerSwaggerDocExtension())->getXsdValidationBasePath());
+    }
+}
---
+++ tests/Functional/Event/OpenAPIDocCreatedEventTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Tests\Functional\Endpoint;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Yoanm\JsonRpcServerDoc\Domain\Model\HttpServerDoc;
+use Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\Event\SwaggerDocCreatedEvent;
+
+/**
+ * @covers \Yoanm\SymfonyJsonRpcHttpServerSwaggerDoc\Event\SwaggerDocCreatedEvent
+ */
+class SwaggerDocCreatedEventTest extends TestCase
+{
+    /** @var SwaggerDocCreatedEvent */
+    private $event;
+
+    /** @var array */
+    private $swaggerDoc;
+    /** @var HttpServerDoc|ObjectProphecy */
+    private $serverDoc;
+
+    protected function setUp()
+    {
+        $this->swaggerDoc = ['swaggerDoc'];
+        $this->serverDoc = $this->prophesize(HttpServerDoc::class);
+
+        $this->event = new SwaggerDocCreatedEvent(
+            $this->swaggerDoc,
+            $this->serverDoc->reveal()
+        );
+    }
+
+    public function testShoulManageSwaggerDocAndServerDoc()
+    {
+        $this->assertSame($this->swaggerDoc, $this->event->getSwaggerDoc());
+        $this->assertSame($this->serverDoc->reveal(), $this->event->getServerDoc());
+    }
+
+    public function testSwaggerDocShouldBeOverridable()
+    {
+        $opeapiDoc = ['my-doc'];
+        $this->event->setSwaggerDoc($opeapiDoc);
+        $this->assertSame($opeapiDoc, $this->event->getSwaggerDoc());
+    }
+}
